8d73fa0ca2fe2bd08bf341a246e2ed93ea6107e7
Siva Dasu <sivadasu88@gmail.com>
2018-07-08 11:40:31 +0530

    Task tracker project added

---
+++ SD.TaskTracker.IdentityServer/ClientStore.cs
@@ -0,0 +1,56 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SD.TaskTracker.IdentityServer
+{
+    public class ClientStore
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+    {
+        new Client
+        {
+            ClientId = "client",
+
+            // no interactive user, use the clientid/secret for authentication
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+            // secret for authentication
+            ClientSecrets =
+            {
+                new Secret("secret".Sha256())
+            },
+
+            // scopes that client has access to
+            AllowedScopes = { "api1" }
+        }
+    };
+        }
+        public static IEnumerable<Client> GetClients1()
+        {
+            return new List<Client>
+    {
+        new Client
+        {
+            ClientId = "client",
+
+            // no interactive user, use the clientid/secret for authentication
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+            // secret for authentication
+            ClientSecrets =
+            {
+                new Secret("secret".Sha256())
+            },
+
+            // scopes that client has access to
+            AllowedScopes = { "api1" }
+        }
+    };
+        }
+    }
+}
---
+++ SD.TaskTracker.IdentityServer/ResourceStore.cs
@@ -0,0 +1,19 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SD.TaskTracker.IdentityServer
+{
+    public class ResourceStore
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+             {
+        new ApiResource("api1", "My API")
+             };
+        }
+    }
+}
