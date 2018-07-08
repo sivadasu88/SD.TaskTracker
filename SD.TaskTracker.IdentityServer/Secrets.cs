de24b5d5a03619a1599034b69db0a9a99c0ab705
Siva Dasu <sivadasu88@gmail.com>
2018-07-08 12:42:59 +0530

    secretes added

---
+++ SD.TaskTracker.IdentityServer/Secrets.cs
@@ -0,0 +1,8 @@
+﻿namespace SD.TaskTracker.IdentityServer
+{
+    public class Secrets
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
